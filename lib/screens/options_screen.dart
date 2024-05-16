16fae2d0c811c6d22e3deba269ff7e0d9021c330
Mutie-Rehman <mutierehman12@gmail.com>
2024-05-16 10:57:41 +0500

    minor changes

--- lib/screens/options_screen.dart
+++ lib/screens/options_screen.dart
@@ -1,6 +1,6 @@
 import 'package:blog_app/assets/buttons/round_button.dart';
 import 'package:blog_app/screens/login_screen.dart';
-import 'package:blog_app/screens/sign_in.dart';
+import 'package:blog_app/screens/register.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
